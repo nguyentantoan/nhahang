19f22c5b395af10634712eafc93e16dcdd451c02
nguyentantoan <51957650+nguyentantoan@users.noreply.github.com>
2021-08-12 12:05:06 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const menuBar = document.querySelector('.menu-bar') 
+menuBar.addEventListener("click", function (){
+    menuBar.classList.toggle("active")
+    document.querySelector(".menu-items").classList.toggle("active")   
+})
+
+const _top = document.querySelector(".top")
+window.addEventListener ("scroll", function(){
+    const y = this.pageYOffset;
+    if (y > 80) {
+        _top.classList.add("active")
+    }else {
+        _top.classList.remove("active")
+    }
+})
+
+const menuTitle = document.querySelector(".menu-title")
+menuTitle.addEventListener("click", function(x){
+    if (x.target.classList.contains("menu-button")){
+        const Target = x.target.getAttribute("data-title")
+        menuTitle.querySelector(".active").classList.remove("active")
+        x.target.classList.add("active")
+        
+        const menuItem = document.querySelector(".menu")
+        menuItem.querySelector(".menu-item-content.active").classList.remove("active")
+        menuItem.querySelector(Target).classList.add("active")
+    }
+})
